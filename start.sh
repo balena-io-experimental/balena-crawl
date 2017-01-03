12fd1d3121ed03ccc938718e0b34c2fa11583c4a
Gergely Imreh <imrehg@gmail.com>
2017-01-03 15:26:52 +0000

    set hostname in start script

--- start.sh
+++ start.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
+# Set device hostname
+# https://docs.resin.io/runtime/runtime/#dbus-communication-with-hostos
+DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host_run/dbus/system_bus_socket \
+  dbus-send \
+  --system \
+  --print-reply \
+  --reply-timeout=2000 \
+  --type=method_call \
+  --dest=org.freedesktop.hostname1 \
+  /org/freedesktop/hostname1 \
+  org.freedesktop.hostname1.SetStaticHostname \
+  string:"${HOSTNAME-crawl}" boolean:true
+
+# Start Crawl server
 cd crawl/crawl-ref/source
 python webserver/server.py
