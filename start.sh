ccfc97bda9186406d22ea2d642800a6d8235c8cc
Gergely Imreh <imrehg@gmail.com>
2017-01-03 15:41:27 +0000

    avahi-publish the server

--- start.sh
+++ start.sh
@@ -13,6 +13,9 @@ DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host_run/dbus/system_bus_socket \
   org.freedesktop.hostname1.SetStaticHostname \
   string:"${HOSTNAME-crawl}" boolean:true
 
+# Avahi advertise the server
+avahi-publish -s "Dungeon Crawl Stone Soup server for $(hostname)" _http._tcp 80 "Dungeon Crawl Stone Soup"
+
 # Start Crawl server
 cd crawl/crawl-ref/source
 python webserver/server.py
